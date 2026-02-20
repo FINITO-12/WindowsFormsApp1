0c6f06586b5a79e6c2b47661976b74422bb088b2
FINITO <finito@rocketship.com>
2026-02-20 17:03:42 +0400

    ysagdywqgdysad

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -133,7 +133,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(252, 293);
+            this.button3.Location = new System.Drawing.Point(488, 168);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
